d8dc742ac32c7ff48fb8eba8ee68d2a05c71a713
Achraf Khamri <achrafkhamri99@gmail.com>
2021-04-30 13:56:52 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function hoverIn(){
+    document.querySelector("img.thumbnail").classList.add("thumbnail-hover");
+}
+
+function hoverOut(){
+    document.querySelector("img.thumbnail").classList.remove("thumbnail-hover");
+}
